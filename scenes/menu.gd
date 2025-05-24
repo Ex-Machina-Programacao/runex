27b945164de503e4dcbe45e713ec0976691b84c5
Ex Machina UNIFEI Oficial <exmachina@unifei.edu.br>
2025-05-24 13:37:39 -0300

    <feat> tela menu
    tela menu modelo básico sem design, cenas de configuração e rank sem nada apenas para implementar funcionalidade do botão do menu

---
+++ scenes/menu.gd
@@ -0,0 +1,9 @@
+extends Control
+
+
+func _on_Configuracao_pressed():
+	get_tree().change_scene("res://scenes/Configuracao.tscn")
+
+
+func _on_Ranking_pressed():
+	get_tree().change_scene("res://scenes/Rank.tscn")
